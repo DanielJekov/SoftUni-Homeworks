cceb1f4243715facdfc52a2690e2e2630719df75
Daniel Zhekov <spyker156@gmail.com>
2021-03-07 16:29:10 +0200

    Adding Exercise 2-3 from homeworks

--- EFCoreLinqExercises/MusicHub/StartUp.cs
+++ EFCoreLinqExercises/MusicHub/StartUp.cs
@@ -1,9 +1,13 @@
 ﻿namespace MusicHub
 {
     using System;
-
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
     using Data;
     using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
 
     public class StartUp
     {
@@ -12,21 +16,87 @@
             MusicHubDbContext context =
                 new MusicHubDbContext();
 
-
-            DbInitializer.ResetDatabase(context);
-
-            //Test your solutions here
+          //DbInitializer.ResetDatabase(context);
+            Console.WriteLine(ExportSongsAboveDuration(context, 4));
         }
 
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
-            throw new NotImplementedException();
+            var Albums = context.Albums
+                .Where(x => x.ProducerId == producerId)
+                .Select(x => new Album
+                {
+                    Name = x.Name,
+                    ReleaseDate = x.ReleaseDate,
+                    Producer = x.Producer,
+                    Songs = x.Songs
+                    .Select(s => new Song
+                    {
+                        Name = s.Name,
+                        Price = s.Price,
+                        Writer = s.Writer
+                    })
+                    .OrderByDescending(s => s.Name)
+                    .ThenBy(s => s.Writer.Name)
+                    .ToList()
+                })
+                .ToList()
+                .OrderByDescending(x => x.Price);
+
+            var sb = new StringBuilder();
+            foreach (var album in Albums)
+            {
+                sb.AppendLine($"-AlbumName: {album.Name}");
+                sb.AppendLine($"-ReleaseDate: {album.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.CreateSpecificCulture("en-US"))}");
+                sb.AppendLine($"-ProducerName: {album.Producer.Name}");
+                sb.AppendLine($"-Songs:");
+
+                int counter = 1;
+                foreach (var song in album.Songs)
+                {
+
+                    sb.AppendLine($"---#{counter++}");
+                    sb.AppendLine($"---SongName: {song.Name}");
+                    sb.AppendLine($"---Price: {song.Price:f2}");
+                    sb.AppendLine($"---Writer: {song.Writer.Name}");
+                }
+
+                sb.AppendLine($"-AlbumPrice: {album.Price:f2}");
+            }
+
+            return sb.ToString().Trim();
         }
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
         {
-            throw new NotImplementedException();
+            var songs = context.Songs
+                .Select(x => new
+                {
+                    SongName = x.Name,
+                    FullName = x.SongPerformers.Select(x => x.Performer.FirstName + " " + x.Performer.LastName).FirstOrDefault(),
+                    WriterName = x.Writer.Name,
+                    AlbumProducer = x.Album.Producer.Name,
+                    Duration = x.Duration
+                })
+                .ToList()
+                .Where(x => x.Duration.TotalSeconds > duration)
+                .OrderBy(x => x.SongName)
+                .ThenBy(x => x.WriterName)
+                .ThenBy(x => x.FullName);
 
+            var sb = new StringBuilder();
+            int count = 1;
+            foreach (var song in songs)
+            {
+                sb.AppendLine($"-Song #{count++}");
+                sb.AppendLine($"---SongName: {song.SongName}");
+                sb.AppendLine($"---Writer: {song.WriterName}");
+                sb.AppendLine($"---Performer: {song.FullName}");
+                sb.AppendLine($"---AlbumProducer: {song.AlbumProducer}");
+                sb.AppendLine($"---Duration: {song.Duration}");
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
